f4fb0bbf56085c9e1a48ca0b5a6272fd9593fd95
bdharmaraj <brainerd.dharmaraj@pnmac.com>
2025-02-11 11:08:07 -0800

    pom.xml updated

--- src/main/java/com/sample/ValidatePaymentEndpoint.java
+++ src/main/java/com/sample/ValidatePaymentEndpoint.java
@@ -1,11 +1,12 @@
 package com.sample;
 
-import javax.inject.Inject;
-import javax.ws.rs.Consumes;
-import javax.ws.rs.POST;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
-import javax.ws.rs.core.MediaType;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.POST;
+import jakarta.ws.rs.Consumes;
+import jakarta.ws.rs.Produces;
+import jakarta.inject.Inject;
+
 
 import org.kie.api.runtime.KieRuntimeBuilder;
 import org.kie.api.runtime.KieSession;
